17106876e6eb154809f496c9925f0d1dbb9b26ef
Dipen <rik.dipen@gmail.com>
2024-04-11 09:50:21 +0530

    Porject added

---
+++ bashScripts/installJava.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt -y install default-jre
+sudo apt -y install default-jdk
+java --version
+
---
+++ projects/demoJava/hello.java
@@ -0,0 +1,7 @@
+public class hello{
+
+public static void main(String Args[]){
+
+	System.out.println("I am Dipen");
+}
+}
