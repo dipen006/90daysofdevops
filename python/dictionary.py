19954f1431ab6b8609d0a8533e8e6fbbcc150035
Dipen <rik.dipen@gmail.com>
2024-04-16 13:03:00 +0530

    Docker Volume made for Django Todo App

---
+++ python/dictionary.py
@@ -0,0 +1,26 @@
+# fav_tools ={
+#   1:"Linux",
+#   2:"Git",
+#   3:"Docker",
+#   4:"Kubernetes",
+#   5:"Terraform",
+#   6:"Ansible",
+#   7:"Chef"
+# }
+# # Define your favorite tool's key
+# favorite_tool_key = 2  # Change this to your favorite tool's key
+
+# # Printing your favorite tool using its key
+# print("My favorite tool is:", fav_tools[favorite_tool_key])
+
+# Create a list of cloud service providers
+cloud_providers = ["AWS", "GCP", "Azure"]
+
+# Add Digital Ocean to the list
+cloud_providers.append("Digital Ocean")
+
+# Sort the list in alphabetical order
+cloud_providers.sort()
+
+# Print the sorted list
+print("Cloud Service Providers (Sorted):", cloud_providers)
