a3d68fb0d4eeb46154b62cc973d844f513a2ca83
Dipen <rik.dipen@gmail.com>
2024-05-24 15:49:22 +0530

    go/inplicitExplicit.go added

---
+++ go/inplicitExplicit.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var number = "5"
+	// number = number + 1
+	// number := do the same things we did just omit the var keyword
+	// number := "String hai boss"
+	// number := 5 cannot do this if you already stored the value as string
+	var number uint64
+	var bl bool
+	fmt.Println(number, bl)
+	// fmt.Printf("%T", number)
+}
